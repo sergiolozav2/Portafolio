f0769916d482fe7ccd4b3bb59f05126da1af1428
sergiolozav <sergiolozav@gmail.com>
2024-02-24 02:18:18 -0400

    feat: Oculta links y muestra boton en 'md'

--- src/modules/Shared/components/Navbar.tsx
+++ src/modules/Shared/components/Navbar.tsx
@@ -2,16 +2,17 @@ import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
 import { IoPlanet } from "react-icons/io5";
 import { AnimatedUnderlineText } from "./AnimatedUnderlineText";
+import { CgMenuGridO } from "react-icons/cg";
 
 export function Navbar() {
   return (
-    <nav className="fixed inset-0 bottom-auto isolate z-10 bg-white/5 bg-clip-padding px-8 py-6 text-lg backdrop-blur-sm backdrop-filter">
+    <nav className="fixed inset-0 bottom-auto isolate z-10 bg-white/5 bg-clip-padding px-6 py-4 text-lg backdrop-blur-sm backdrop-filter md:px-8 md:py-6">
       <div className="relative flex w-full items-center justify-between font-medium text-stone-100">
         <div className="flex gap-4 text-xl">
           <IoPlanet className="text-2xl" />
           <span>LozaDev</span>
         </div>
-        <div className="flex gap-4 text-2xl">
+        <div className="hidden gap-4 text-2xl md:flex">
           <a href="https://www.linkedin.com/in/sergio-loza/" target="_blank">
             <FaLinkedin />
           </a>
@@ -20,12 +21,12 @@ export function Navbar() {
           </a>
         </div>
 
-        <div className="absolute right-1/2 top-0 flex translate-x-1/2 gap-8">
+        <div className="absolute right-1/2 top-0 hidden translate-x-1/2 gap-8 md:flex">
           <AnimatedUnderlineText>
             <button>Inicio</button>
           </AnimatedUnderlineText>
           <AnimatedUnderlineText>
-            <button>Acerca de mí</button>
+            <button className="whitespace-nowrap">Acerca de mí</button>
           </AnimatedUnderlineText>
           <AnimatedUnderlineText>
             <button>Experiencia</button>
@@ -34,6 +35,9 @@ export function Navbar() {
             <button>Proyectos</button>
           </AnimatedUnderlineText>
         </div>
+        <button className="block text-3xl md:hidden">
+          <CgMenuGridO />
+        </button>
       </div>
     </nav>
   );
