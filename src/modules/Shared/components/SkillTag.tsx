0d10dbe803f23eb57b67477f496957168e17245b
Sergio Loza <sergiolozav@gmail.com>
2024-04-02 10:38:57 -0400

    feat: Componente para tag de habilidades

---
+++ src/modules/Shared/components/SkillTag.tsx
@@ -0,0 +1,40 @@
+export interface SkillTagProps {
+  text: string;
+}
+
+export function SkillTag(props: SkillTagProps) {
+  return (
+    <div
+      className="rounded-md px-1 py-0.5 text-sm font-semibold text-stone-100 sm:text-base"
+      style={{
+        backgroundColor: mapSkillToColor(props.text),
+      }}
+    >
+      {props.text}
+    </div>
+  );
+}
+
+function mapSkillToColor(name: string) {
+  return (
+    {
+      React: "#2d7c9c",
+      Javascript: "#d97706",
+      HTML: "#e45132",
+      CSS: "#9333ea",
+      Tailwind: "#1d4ed8",
+      Git: "#44403c",
+      Github: "#57534e",
+      Express: "#15803d",
+      NestJS: "#0c0a09",
+      Linux: "#c2410c",
+      SQL: "#dc2626",
+      Wordpress: "#0284c7",
+      Cypress: "#2f8e65",
+      Flutter: "#125D9F",
+      Dart: "#175384",
+      Jest: "#B44263",
+      Firebase: "#ce722b",
+    }[name] ?? "#0e7490"
+  );
+}
