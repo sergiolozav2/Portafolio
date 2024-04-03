1a2896a29aee1108c43dbcb1e2d68f225083e8cf
Sergio Loza <sergiolozav@gmail.com>
2024-04-02 21:25:43 -0400

    fix: Fecha incorrecta de experiencia

--- src/modules/Home/Components/Experience/ExperienceSection.tsx
+++ src/modules/Home/Components/Experience/ExperienceSection.tsx
@@ -8,13 +8,13 @@ export function ExperienceSection() {
       <div className="flex flex-col gap-12">
         <ExperienceCard
           title="Desarrollador Fullstack · Cinavar"
-          timeframe="Nov 2023 - Feb 2024"
+          timeframe="Sep 2023 - Feb 2024"
           description="Desarrollé un sistema de seguridad víal para la inspección de operaciones logísticas en camiones cisternas. Colaboré con inspectores de seguridad vial para las etapas de diseño e implementación."
           skills={["React", "Express", "Git", "Github Actions"]}
         />
         <ExperienceCard
           title="Desarrollador Frontend · SoyYo Digital"
-          timeframe="Feb 2023 - Nov 2023"
+          timeframe="Feb 2023 - Sep 2023"
           description="Diseñé y desarrollé la interfaz de SoyYoDigital para tarjetas digitales de presentación. Adicionalmente utilicé wordpress para manejar la página de la empresa."
           skills={["React", "Jest", "Wordpress", "Git"]}
         />
