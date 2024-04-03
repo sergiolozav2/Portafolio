e7cf41bba35ed38ff9038e26d9ebded81be46bb5
Sergio Loza <sergiolozav@gmail.com>
2024-04-02 21:25:16 -0400

    feat: Extendido tailwind con color de navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        navbar: "#090a0f",
+      },
+      screens: {
+        "-md": { max: "767px" },
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
