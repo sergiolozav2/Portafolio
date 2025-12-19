c735198b85e9f13297142b64b62b10f43e9edb3b
Sergio Loza <sergiolozav@gmail.com>
2025-12-19 17:59:17 -0400

    Add clarity

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { Navbar } from "./modules/Shared/components/Navbar";
 import { HomePage } from "./modules/Home/HomePage";
+import Clarity from "@microsoft/clarity";
+
+const projectId = "uo5tzg3uw8";
+
+Clarity.init(projectId);
 
 function App() {
   return (
